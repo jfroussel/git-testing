e35a8eb469f35d76d28e89423e5a6645297acf83
jeff roussel <jfroussel@gmail.com>
2019-06-02 11:42:00 +0200

    add Utils to testing with enzime and create first test

--- src/components/jeffy/index.js
+++ src/components/jeffy/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Jeffy = () => {
     return (
         <div data-test="jeffyComponent">
-            <h2>Welcome jeffy</h2>
+            <h2 data-test="title">Welcome jeffy</h2>
         </div>
     );
 };
